8602298f6dfe027ab85722e9fa1a8dc67a051476
Shane D <111009039+Axycr-06@users.noreply.github.com>
2026-09-04 00:06:58 +0000

    fixed proper styling to word_grid

--- word_grid.py
+++ word_grid.py
@@ -1,3 +1,11 @@
+"""
+    File: word_grid.py
+    Author: Shane Dyke
+    Course: CSC 120, Fall 2026
+    Purpose: This program takes two inputs; size of grid, and random_seed
+    it then creates a grid of random letters
+"""
+
 import random
 import string
 
@@ -11,22 +19,21 @@ def init():
 def make_grid(grid_size):
     grid = []
 
+    #create empty row first so that it can be filled
     for i in range(grid_size):
-        #create empty row first so that it can be filled
         row = []
 
         #loop through the grid size to create the columns
         for t in range(grid_size):
-            #append a random letter to the row
-            row.append(random.choice(string.ascii_lowercase))
+            row.append(random.choice(string.ascii_lowercase)) #append a random letter to the row
 
-        #add the row to the grid
-        grid.append(row)
+       
+        grid.append(row) #add the row to the grid
 
     return grid
 
 def print_grid(grid):
     for row in grid:
-        print(",".join(row))
+        print(",".join(row)) #add a comma between each letter
 
 print_grid(make_grid(init()))
